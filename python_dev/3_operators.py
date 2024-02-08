2041860c477d5b0b5526d064330b8d9d2b40c420
derek <derekthedevil69@gmail.com>
2024-02-09 03:45:39 +0530

    nothing interesting

---
+++ python_dev/3_operators.py
@@ -0,0 +1,52 @@
+# operators : are tools /symbols used to perform operations on data 
+# ACALINB : All Cricketers Are Living In Mumbai 
+
+# Arithmetic : +,-,*,/,//,%,** 
+# Comparison : <,>,<=,>=,==,!= 
+
+
+
+
+print(10+2)
+print(10-2)
+print(10*2)
+print(10/2)
+print(10.0/2)
+print(10%2)
+print(10%3)
+print(10//2)
+print(10//3)
+print(12//5)
+print(-10//3)
+print(10**2)
+
+
+
+
+
+
+
+
+print(10>2)
+print(10<2)
+print(10>=2)
+print(10<=2)
+print(10!=2)
+print(10==2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
